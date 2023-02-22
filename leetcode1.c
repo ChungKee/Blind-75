5837d7645bb477ca772927ef5e2ed1ea654d0008
ChungKee <47098015+ChungKee@users.noreply.github.com>
2023-02-22 17:39:43 +0800

    Update leetcode1.c

--- leetcode1.c
+++ leetcode1.c
@@ -1,1 +1,16 @@
-
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int* temp = NULL;
+    *returnSize = 0;
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i + 1; j < numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                *returnSize = 2;
+                temp = (int*)malloc(sizeof(int) * 2);
+                temp[0] = i;
+                temp[1] = j;
+                break;
+            }
+        }
+    }
+    return temp;
+}
